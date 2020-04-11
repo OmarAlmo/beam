42715e4e6d53d65161a62dfe59ac35cbded57749
OmarAlmo <omar.almoallim@gmail.com>
2020-04-11 00:25:16 -0400

    close file

--- middleware/reuters_pre_process.py
+++ middleware/reuters_pre_process.py
@@ -56,7 +56,7 @@ def valid_term(term):
 def build_index():
     retuersIndexFile = open('./reuters_index.csv', 'w')
     retuersIndex = csv.writer(retuersIndexFile)
-    retuersIndex.writerow(['docID', 'title', 'topics','author', 'date', 'body'])
+    retuersIndex.writerow(['docID', 'title', 'topics','author', 'date', 'body', 'topics'])
     docID = 0
     for file in glob.glob(os.path.join(reutersDir, '*.sgm')):
         print(file)
@@ -95,7 +95,8 @@ def build_index():
                 retuersIndex.writerow(
                     [docID, title, topics, author, date, body])
                 docID += 1
-
+    
+    f.close()
     retuersIndexFile.close()
 
 def bigram_helper(term):
@@ -138,7 +139,6 @@ def build_bigram():
             id += 1
 
     reutersBigramFile.close()
-build_bigram()
 
 def reduce_bigram():
     df = pd.read_csv('./reuters_bigram.csv', index_col=0, header=0)
@@ -149,7 +149,6 @@ def reduce_bigram():
             df.drop(df.index[i])
             print("dropeed")
     df.to_csv('reuters_bigram_2.csv')
-# reduce_bigram()
 
 def build_dictionary():
     index = pd.read_csv('./reuters_index.csv', header=0)
